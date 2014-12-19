2db60d5233434d0060023c309b79ae0c59174186
Roman Mazur <roman.mazur@hotmail.com>
2014-12-19 11:46:37 +0100

    Pridani podpory pro testovani rect component plus vytvoreni prazdneho testu pro Users componentu

---
+++ js/components/__tests__/Users.react-test.js
@@ -0,0 +1,8 @@
+jest.dontMock('../Users.react');
+
+describe('Users component', function () {
+	it('renders with no data initially', function () {
+		var React = require('react');
+		expect(2).toBe(2);
+	});
+});
